940c5ecb027d1a942b6e09121ca360a1252502a2
Siva Chowdeswar Nandipati <Nandipatisivachowdeswar@gmail.com>
2023-06-06 00:34:31 -0600

    Syling Orginzer button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import Form from "./components/Form";
 import OrganizerDetails from "./OrganizerDetails";
+import "./App.css";
 
 const App = () => {
   const [organizerDetails, setOrganizerDetails] = useState(false);
+
   if (organizerDetails) return <OrganizerDetails />;
+
   return (
     <div>
       <Form />
-      <button onClick={() => setOrganizerDetails(true)}>
+      <button
+        className="button" // Add the class name for styling
+        onClick={() => setOrganizerDetails(true)}
+      >
         Open OrganizerDetails.js
       </button>
     </div>
